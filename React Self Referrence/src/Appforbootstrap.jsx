b40bbae5ca77d5d61f08b8055dcae8556cce6ecc
Roshan1115 <roshanbishi7890@gamil.com>
2021-04-20 16:22:38 +0530

    More about bootstrap in react

--- React Self Referrence/src/Appforbootstrap.jsx
+++ React Self Referrence/src/Appforbootstrap.jsx
@@ -4,7 +4,7 @@ import '../node_modules/bootstrap/dist/css/bootstrap.css'
 const AppBootstrap = () => {
   return(
     <>
-  <h1>Inside app for bootstrap file.</h1>
+  <h1 className="text-center" >Inside app for bootstrap file.</h1>
   <button className="btn btn-primary"> My first bootstrap button on react</button>
     </>
   )
