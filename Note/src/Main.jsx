510d5cd4ab1eee8e39eb4ede0dd5f67015ca6722
Roshan1115 <roshanbishi7890@gamil.com>
2021-04-24 14:22:15 +0530

    Added the add button working

--- Note/src/Main.jsx
+++ Note/src/Main.jsx
@@ -1,24 +1,56 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Tooltip from '@material-ui/core/Tooltip';
 import AddIcon from '@material-ui/icons/Add';
 import Fab from '@material-ui/core/Fab';
-import DeleteIcon from '@material-ui/icons/Delete';
-import IconButton from '@material-ui/core/IconButton';
+import Note from './Note'
 
 
 
 const Main = () => {
 
+  const [input, setInput] = useState({
+    title: "",
+    content: ""
+  });
+  const [noteArr, setNoteArr] = useState([]);
 
+  // take input into the state  
+  const takeInput = (e) => {
+    setInput((prev) => {
+      return {
+        ...prev,
+        [e.target.name]: e.target.value
+      }
+    })
+  }
+
+  // Add note to the page
+  const addNote = () => {
+    setNoteArr((prev) => {
+      return [...prev, input]
+    })
+    setInput({
+      title: "",
+      content: ""
+    })
+  }
+
+  //change focus from title to content field 
+  const changeFocus = (e) => {
+      if(e.key === "Enter" && e.target.value !== ""){
+        const inputContent = document.querySelector("#content")
+        inputContent.focus();
+      }
+  }
 
   return (
     <>
       <div className="main_container">
         <div className="input_container">
           <div className='input_holder'>
-            <input type="text" placeholder='Tittle' />
-            <textarea placeholder='Write note' />
-            <Tooltip title="Add" aria-label="add" id="myAdd_btn">
+            <input type="text" id="title" placeholder='Tittle' name="title" value={input.title} onChange={takeInput} onKeyDown={changeFocus} />
+            <textarea placeholder='Write note' id="content" name="content" value={input.content} onChange={takeInput} />
+            <Tooltip title="Add" aria-label="add" id="myAdd_btn" onClick={addNote}>
               <Fab color="primary" >
                 <AddIcon />
               </Fab>
@@ -28,16 +60,15 @@ const Main = () => {
 
         <div className="NoteList_container">
 
-          <div className='NoteList'>
-            <h3>Title</h3>
-            <p>this is my note taken here.I am witting this to create my own note taking app.pieihshhehhigweijwpeirjw;or ierw pwier g pwir g qr pqirie pre eriewhh prwer prh </p>
-            <Tooltip title="Delete">
-              <IconButton aria-label="delete">
-                <DeleteIcon />
-              </IconButton>
-            </Tooltip>
-          </div>
-         
+          {noteArr.map((currentValue, index) => {
+            return (<Note
+              key={index}
+              id={index}
+              title_h3={currentValue.title}
+              content_p={currentValue.content}
+            />)
+          })}
+
         </div>
       </div>
     </>
---
+++ Note/src/Note.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import DeleteIcon from '@material-ui/icons/Delete';
+import IconButton from '@material-ui/core/IconButton';
+import Tooltip from '@material-ui/core/Tooltip';
+
+
+const Note = (props) => {
+
+  return (
+    <>
+      <div className='NoteList'>
+        <h3>{props.title_h3}</h3>
+        <p>{props.content_p}</p>
+        <Tooltip title="Delete">
+          <IconButton aria-label="delete">
+            <DeleteIcon />
+          </IconButton>
+        </Tooltip>
+      </div>
+    </>
+  )
+}
+
+export default Note;
