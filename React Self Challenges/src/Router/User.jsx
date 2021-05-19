7bc6236479a0afc95702b13757f5e61ff38b9612
Roshan1115 <roshanbishi7890@gamil.com>
2021-05-19 20:49:28 +0530

    useLocation introduction

--- React Self Challenges/src/Router/User.jsx
+++ React Self Challenges/src/Router/User.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
-import { useParams } from 'react-router';
+import { useLocation, useParams } from 'react-router';
 
 const User = () => {
   const {firstName,lastName} = useParams();    // the name must be same as in path.
+
+  const urlLocation = useLocation();
   
-  return <h1>Hello <span style={{color: 'red'}}>User is</span><span style={{color:'blue'}}> {firstName} {lastName}</span>.</h1>
+  return <>
+  <h1>Hello <span style={{color: 'red'}}>User is</span><span style={{color:'blue'}}> {firstName} {lastName}</span>.</h1>
+  <h2>you are in <span style={{color: 'blue'}}>{urlLocation.pathname}</span></h2>
+  </>
 }
 export default User;
