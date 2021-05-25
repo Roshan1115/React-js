9a289ddfcc134e1554b4d569aa5ff5744d0f407a
Roshan1115 <roshanbishi7890@gamil.com>
2021-05-25 17:48:31 +0530

    Redirect functioning

--- React Self Challenges/src/Router/App.jsx
+++ React Self Challenges/src/Router/App.jsx
@@ -3,18 +3,11 @@ import About from './About'
 import Contact from './Contact'
 import User from './User'
 import Search from './Search'
-import {Route, Switch} from 'react-router-dom'
+import {Route, Switch, Redirect} from 'react-router-dom'
 
 
 const AppRout = () => {
-
-  const errorCom = () => {
-    return(
-      <h1>This is the default url.</h1>
-    )
-  }
-
-
+ 
   // we can even yse render methode or component methode
   // render methode serve the component only once and the compo stays in user browser
   // conponent methode serve the conponent every time user request.
@@ -27,7 +20,11 @@ const AppRout = () => {
     <Route exact path='/contact' render={() => <Contact name="Roshan" />} /> 
     <Route path='/user/:firstName/:lastName' component={User} />
     <Route exact path='/search' component={Search} />
-    <Route component={errorCom} />
+
+
+    {/* Now with redirect methode we don't need to make error page. If user type something wrond in urll bar then it will redirected  */}
+
+    <Redirect to='/' />
     </Switch>
     </>
   )
